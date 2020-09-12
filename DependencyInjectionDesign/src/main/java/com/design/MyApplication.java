45c05c9c2800ce7f9177b711121e9710b0077a1f
Sunandan Bose <sunandan.riders@gmail.com>
2020-09-12 16:32:34 +0530

    Dependency Injection Task

--- DependencyInjectionDesign/src/main/java/com/design/MyApplication.java
+++ DependencyInjectionDesign/src/main/java/com/design/MyApplication.java
@@ -1,13 +1,25 @@
 package com.design;
 
-import com.design.consumer.Consumer;
-import com.design.service.EmailService;
 
-public class MyApplication implements Consumer {
+import java.util.List;
 
-    private EmailService email;
+public class MyApplication {
 
-    public void processMessages(String msg, String rec){
-        this.email.sendEmail(msg, rec);
-    }
+   
+    private String objectCreationPackage = "com.design";
+
+
+
+    //scan the defined package
+    //Identify between interface and class
+    //create Objects of the identified java class and store it in a container
+    //expose a method which will take class name in string and return object
+
+    //main
+    // run the above steps
+    // create object of MessagingApplicton(get Object with the help of DI)
+    // call .processMessages from messaging Application
+
+
+
 }
